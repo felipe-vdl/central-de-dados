8b249f9c228b225c19ad07051d1f80467485e98d
felipe-pmm <felipe.vidal.mesquita@gmail.com>
2023-06-04 11:14:39 -0300

    UI

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Fogo from "./Fogo";
 
 export default function Footer() {
   return (
-    <footer className={`z-10 bg-roxo py-1 px-4 text-white border-t border-white/20`}>
+    <footer className="z-20 bg-roxo py-1 px-4 text-white border-t border-white/20">
       <ul className="flex items-center justify-between text-center text-[11px] w-full max-w-[1440px] mx-auto">
         <li>© 2023 Equipe de Desenvolvimento de Sistemas</li>
         <li>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
-    <div className="flex-1 flex justify-evenly flex-wrap gap-4 p-4">
+    <div className="flex-1 flex justify-evenly flex-wrap gap-x-4 gap-y-8 p-4">
       {Object.keys(dashboardList).map(dashboard => (
-        <div key={crypto.randomUUID()} className="flex justify-center self-start w-full md:w-1/3 lg:w-1/4">
+        <div key={crypto.randomUUID()} className="flex justify-center self-start w-full md:w-1/4 lg:w-1/5">
           <Link to={dashboard} className="p-4 flex flex-col items-center rounded hover:bg-slate-200 transition-colors">
             <img width={256} alt={dashboard.toUpperCase()} className="aspect-square object-cover object-center" src={dashboardList[dashboard].logo} />
-            <p className="font-bold bg-roxo text-white w-1/3 text-center p-1 rounded mt-2">{dashboard.toUpperCase()}</p>
+            <p className="font-bold bg-roxo text-white w-full md:w-4/5 text-center p-1 rounded mt-2">{dashboard.toUpperCase()}</p>
           </Link>
         </div>
       ))}
