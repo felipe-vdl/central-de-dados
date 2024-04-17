feddac03cd07a9f8d449f48375d8c298e2528551
Felipe Vidal <vidalfelipe2018@gmail.com>
2024-04-17 18:58:12 -0300

    UI

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -4,9 +4,9 @@ import Footer from "./Footer";
 
 export default function RootLayout() {
   return (
-    <div className="flex flex-col min-h-screen font-">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-1 flex flex-col bg-white bg-gradient-to-br">
+      <main className="flex-1 min-h-screen sm:min-h-full flex flex-col bg-white bg-gradient-to-br">
         <Outlet />
       </main>
       <Footer />
